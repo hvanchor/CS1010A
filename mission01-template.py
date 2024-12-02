01d21567fbedad687c10bb45144813c43d710352
hvanchor <122733186+hvanchor@users.noreply.github.com>
2024-12-02 19:58:30 +0800

    Add files via upload

---
+++ mission01-template.py
@@ -0,0 +1,36 @@
+#
+# CS1010A --- Programming Methodology
+#
+# Mission 1
+#
+# Note that written answers are commented out to allow us to run your
+# code easily while grading your problem set.
+
+from runes import *
+
+
+##########
+# Task 1 #
+##########
+
+def mosaic(pic1, pic2, pic3, pic4):
+    return beside(stack(pic3,pic4),stack(pic1,pic2))
+
+
+# Test
+show(mosaic(rcross_bb, sail_bb, corner_bb, nova_bb))
+
+##########
+# Task 2 #
+##########
+
+def simple_fractal(pic):
+    return beside(pic, stack(pic,pic))
+
+# Test
+#show(simple_fractal(make_cross(rcross_bb)))
+
+
+
+
+#show(beside(heart_bb, stack(heart_bb,heart_bb)))
